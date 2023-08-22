a7eeb0b59ec50e74241345fbb4a0e6516b45312e
ssl1203 <seansleu@gmail.com>
2023-08-22 12:39:21 -0700

    QQQ % format

--- mk_summary.py
+++ mk_summary.py
@@ -515,7 +515,7 @@ def update_summary_sheet():
         ws_summary[f'{chr(col_id+1)}{row_id}'].value = round(mega8_value_dict[f'{row}'],2)
 
         if row != 'TSM':
-            ws_summary[f'{chr(col_id+3)}{row_id}'].value = round(qqq_top_10_dict[f'{row}'],2)
+            ws_summary[f'{chr(col_id+3)}{row_id}'].value = qqq_top_10_dict[f'{row}']
 
         row_id+=1
 ##################################################################
